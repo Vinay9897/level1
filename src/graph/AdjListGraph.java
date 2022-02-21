871ae7f11d0fbc7bb814a7f767c084fd9d8fae28
Vinay <vinayyadav91190@gmail.com>
2022-02-22 00:06:49 +0530

    Implement the graph using Linked List

--- src/graph/AdjListGraph.java
+++ src/graph/AdjListGraph.java
@@ -11,22 +11,39 @@ public class AdjListGraph {
     public AdjListGraph(int nodes) {
         this.V = nodes;
         this.E = 0;
-        // this.adj = new LinkedList[nodes];
+        this.adj = new LinkedList[nodes];
         for (int v = 0; v < V; v++) {
-            // adj[v] = new LinkedList<>();
+            adj[v] = new LinkedList<>();
         }
     }
 
     public void addEdge(int u, int v) {
-        // adj[u].add(v);
-        // adj[v].add(u);
+        adj[u].add(v);
+        adj[v].add(u);
         E++;
 
     }
 
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(V + " Vertices, " + E + " Edges " + "\n");
+        for (int v = 0; v < V; v++) {
+            sb.append(v + ": ");
+            for (int w : adj[v]) {
+                sb.append(w + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         AdjListGraph g = new AdjListGraph(4);
-
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+        System.out.println(g);
     }
 
 }
